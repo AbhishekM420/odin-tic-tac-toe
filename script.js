6305a3fb8528a5af4f3ea4afcaf20c16f77e8bc5
AbhishekM420 <120887834+AbhishekM420@users.noreply.github.com>
2023-01-31 14:19:59 +0530

    Add files via upload
    made the game logic into a separate function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //set up the gameboard module
 let gameboardModule = function() {
-    let gameboard = ["X"];
+    let gameboard = [];
     return {gameboard};
     
 }();
@@ -8,16 +8,16 @@ let gameboardModule = function() {
 //setting up display controller module
 
 let displayControlModule = (function(){
-   const clickBoxes = document.querySelectorAll(".cb");
+   const clickBoxes =   document.querySelectorAll(".cb");
    
    for(let i =0; i < 9; i++){
-    clickBoxes[i].value = i;
     clickBoxes[i].addEventListener("click", gmBrd, false );  //sends the index number to be accessed by this
-    console.log(clickBoxes[i].value);
+    //console.log(clickBoxes[i].value);
+  
 
    }
    
-   
+
 
 } )();
 
@@ -29,11 +29,35 @@ let createPlayer =(playerName, playerNumber, chosenSign) => {
     return {getPlayerName,playerName,playerNumber,chosenSign};
 
 }
-
+ let i = 0;
  function gmBrd(){
+    
     let grdbx = document.querySelectorAll(".box");
-    grdbx[this.value].textContent = gameboardModule.gameboard; // gets the index number
-};
+
+    if(i % 2 == 0)
+    gameboardModule.gameboard[this.value] = "X";
+    else{
+     gameboardModule.gameboard[this.value] = "O" ;
+    }
+    
+    
+    grdbx[this.value].textContent = gameboardModule.gameboard[this.value]; // gets the index number
+   console.log(gameboardModule.gameboard) 
+    i++;      // alternate between X and O
+    gamePlay();
+}
 
 let abhishek = createPlayer("abhishek", 1, "x");
 let M = createPlayer("m", 2, "o");
+ 
+function gamePlay(){   //adding game logic
+    
+    for(let j = 0; j < 9; j++){
+      if(gameboardModule.gameboard[j] == "X")
+      if (  (gameboardModule.gameboard[j] == gameboardModule.gameboard[j+1] && gameboardModule.gameboard[j+1]==gameboardModule.gameboard[j+2]) ||(gameboardModule.gameboard[j] == gameboardModule.gameboard[j+3] && gameboardModule.gameboard[j+3] == gameboardModule.gameboard[j+6]))
+      console.log(" X wins")  ;
+      else if(gameboardModule.gameboard[j] == "O")
+      if (  (gameboardModule.gameboard[j] == gameboardModule.gameboard[j+1] && gameboardModule.gameboard[j+1]==gameboardModule.gameboard[j+2]) ||(gameboardModule.gameboard[j] == gameboardModule.gameboard[j+3] && gameboardModule.gameboard[j+3] == gameboardModule.gameboard[j+6]))
+      console.log(" O wins")  ;
+    }
+}
